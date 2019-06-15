856fe34af23dd7e55144138a28f25d86b47d2e56
Sheldon Johnson <shayolden@hotmail.com>
2019-06-16 02:42:09 +1000

    Remove old individual updaters

--- map-repo/sync.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-rsync -atzv rsync://fortressone.quakerepo.net/maps /fortressonesv
--- server-qwprogs/sync.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-curl -L \
-  -o /fortressonesv/fortress/qwprogs.dat \
-  -z /fortressonesv/fortress/qwprogs.dat \
-  https://github.com/FortressOne/server-qwprogs/releases/latest/download/qwprogs.dat
